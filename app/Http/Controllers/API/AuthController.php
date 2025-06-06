e0eb5d3612c8f531da85238a3bc5f503cc8648a1
MrFatra <ramadhanfatra2004@gmail.com>
2025-06-06 19:36:07 +0700

    feat: add phone_number field to user registration

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -27,6 +27,7 @@ class AuthController extends Controller
                 'name' => $request->name,
                 'email' => $request->email,
                 'password' => Hash::make($request->password),
+                'phone_number' => $request->phone_number,
                 'role' => $request->role,
             ]);
 
